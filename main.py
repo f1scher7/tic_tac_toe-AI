b58795f66f74c794411e31ddcdb71947c3ae1020
f1scher7 <maksym.szylo@gmail.com>
2023-07-02 21:47:33 +0200

    add Game class and implement grid drawing method show_lines()

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ pygame.display.set_caption("Tic Tac Toe AI")
 screen.fill(BG_COLOR)
 
 
+class Game:
+
+    def __init__(self):
+        self.show_lines()
+
+    def show_lines(self):
+        # vertical
+        pygame.draw.line(screen, LINE_COLOR, (SQ_SIZE, 0), (SQ_SIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (2 * SQ_SIZE, 0), (2 * SQ_SIZE, HEIGHT), LINE_WIDTH)
+        # horizontal
+        pygame.draw.line(screen, LINE_COLOR, (0, SQ_SIZE), (WIDTH, SQ_SIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, 2 * SQ_SIZE), (WIDTH, SQ_SIZE * 2), LINE_WIDTH)
+
+
 def main():
+
+    game = Game()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
