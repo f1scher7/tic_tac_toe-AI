5fc0945ce1a8c39c531ff86228254dbaba2f9ad5
f1scher7 <maksym.szylo@gmail.com>
2023-07-03 00:22:43 +0200

    add parameters for cross and circle

--- constans.py
+++ constans.py
@@ -4,10 +4,19 @@ HEIGHT = 600
 
 ROWS = 3
 COLS = 3
+
+# sizes of figures
 SQ_SIZE = WIDTH // COLS
 
+# widths of figures
 LINE_WIDTH = 15
+CROSS_WIDTH = 20
+OFFSET = 30
+CIRCLE_WIDTH = 15
+RADIUS = SQ_SIZE // 2.5
 
 # colors
 BG_COLOR = (28, 170, 156)
 LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
